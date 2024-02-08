593568f1709553f2a8d218375da0628a1ec80992
Chris Curl <curlbud-github@yahoo.com>
2024-02-07 21:45:54 -0500

    Update main.c

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ void printStringF(const char *fmt, ...) {
     char buf[256];
     va_list args;
     va_start(args, fmt);
-    vsnprintf(buf, 200, fmt, args);
+    vsnprintf(buf, sizeof(buf), fmt, args);
     va_end(args);
     printString(buf);
 }
